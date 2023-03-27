b2116cc8dfde9b6e0e5e2b3d748e8b427f80ebb8
BITNA GU <126743003+starirene9@users.noreply.github.com>
2023-03-27 10:38:24 +0900

    initial commit

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        String name = "김철수";
+
+        double rn = Math.random();
+
+      Scanner sc =  new Scanner(System.in);
+
+    }
+}
